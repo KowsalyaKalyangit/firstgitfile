b3e5b6d60c7f4c0cb3ea8ea685ba7085036c2e45
KowsiKalyan <kowsalyacherritech@gmail.com>
2023-05-23 14:38:37 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/forceupdate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.forceupdate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'offline_downloads.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Firstpage(),
+    );
+  }
+}
+
+class Firstpage extends StatefulWidget {
+  const Firstpage({super.key});
+
+  @override
+  State<Firstpage> createState() => _FirstpageState();
+}
+
+class _FirstpageState extends State<Firstpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Text('updating version..........')],
+    );
+  }
+}
